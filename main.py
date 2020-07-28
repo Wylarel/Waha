cdb2f5733c783083b892a749d8809855027d5222
Wylarel <timjemine@gmail.com>
2020-07-28 22:01:51 +0200

    Add notes

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+import random
+import string
 from datetime import datetime
 
 from kivy.properties import ObjectProperty
 from kivy.storage.jsonstore import JsonStore
 from kivy.uix.boxlayout import BoxLayout
-
 from kivymd.app import MDApp
 from kivymd.uix.button import MDFlatButton
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.gridlayout import MDGridLayout
+from kivymd.uix.list import OneLineIconListItem, IconLeftWidget
 from kivymd.uix.picker import MDDatePicker
 
 
@@ -24,8 +26,14 @@ class ClassDialog(BoxLayout):
     pass
 
 
+class NoteListItem(OneLineIconListItem):
+    icon = "trash-can"
+
+
 class Waha(MDGridLayout):
     dialog = None
+    current_note = "new"
+    notelist_items = {}
 
     def update_info(self):
         store = JsonStore('info.json')
@@ -114,6 +122,42 @@ class Waha(MDGridLayout):
     def dialog_close(self, *args):
         if self.dialog:
             self.dialog.dismiss(force=True)
+
+    def load_note(self, *args):
+        if args[0] in self.notelist_items.keys():
+            note_to_load = self.notelist_items.get(args[0])
+        else:
+            note_to_load = ''.join(random.choice(string.ascii_lowercase) for i in range(32))
+        store = JsonStore('notes.json')
+        if note_to_load in store.keys():
+            self.ids["note_textfield"].text = store.get(note_to_load).get("text")
+        else:
+            self.ids["note_textfield"].text = ""
+
+        self.ids["screen_manager"].current = "editnote"
+        self.current_note = note_to_load
+
+    def save_note(self):
+        text = self.ids["note_textfield"].text
+        if text.replace(" ", "") == "": return
+        store = JsonStore('notes.json')
+        store.put(self.current_note, text=text)
+
+    def update_notelist(self):
+        store = JsonStore('notes.json')
+        notelist = self.ids["notelist"]
+        for widget in self.notelist_items:
+            notelist.remove_widget(widget)
+        self.notelist_items = {}
+
+        for note in store.keys():
+            widget = NoteListItem(text=str(store.get(note).get("text")))
+            notelist.add_widget(widget)
+            self.notelist_items[widget] = note
+
+    def delete_note(self, *args):
+        JsonStore('notes.json').delete(self.notelist_items.get(args[0]))
+        self.update_notelist()
 
 
 class MainApp(MDApp):
